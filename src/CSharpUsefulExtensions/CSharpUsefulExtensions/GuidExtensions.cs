c6e157002977e0a19070ff90aec133a5c4672d30
Kazimierz Matyaszek <kazimierz.matyaszek@gmail.com>
2018-04-22 21:06:31 +0200

    Guid: IsEmpty

---
+++ src/CSharpUsefulExtensions/CSharpUsefulExtensions/GuidExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpUsefulExtensions
+{
+    public static class GuidExtensions
+    {
+        public static bool IsEmpty(this Guid guid)
+        {
+            return guid == Guid.Empty;
+        }
+    }
+}
---
+++ test/CSharpUsefulExtensions.Test/GuidExtensions.Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace CSharpUsefulExtensions.Test
+{
+    [TestFixture]
+    public class GuidExtensions
+    {
+        [Test]
+        public void IsEmpty_EmptyGuid_ShouldReturnTrue()
+        {
+            Guid guid;
+
+            var isEmpty = guid.IsEmpty();
+
+            isEmpty.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsEmpty_NotEmptyGuid_ShouldReturnFalse()
+        {
+            Guid guid = Guid.NewGuid();
+
+            var isEmpty = guid.IsEmpty();
+
+            isEmpty.Should().BeFalse();
+        }
+    }
+}
