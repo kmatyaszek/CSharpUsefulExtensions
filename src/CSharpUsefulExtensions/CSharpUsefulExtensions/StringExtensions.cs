d25d991d4563ad4917b2213bbcc1096fb7ce5327
Kazimierz Matyaszek <kazimierz.matyaszek@gmail.com>
2018-04-27 09:16:09 +0200

    String extensions: Left, Right

---
+++ src/CSharpUsefulExtensions/CSharpUsefulExtensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpUsefulExtensions
+{
+    public static class StringExtensions
+    {
+        public static string Left(this string value, int length)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+
+            return value.Length <= length ? value : value.Substring(0, length);
+        }
+
+        public static string Right(this string value, int length)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+
+            return length >= value.Length ? value : value.Substring(value.Length - length);
+        }
+    }
+}
---
+++ test/CSharpUsefulExtensions.Test/StringExtensionsTests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace CSharpUsefulExtensions.Test
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void Left_EmptyString_ShouldReturnEmptyString()
+        {
+            var value = "";
+
+            var result = value.Left(5);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Left_NullString_ShouldReturnNullString()
+        {
+            string value = null;
+
+            var result = value.Left(5);
+
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void Left_StringWithLowerLengthThanGivenLength_ShouldReturnEntireString()
+        {
+            string value = "abcdef";
+
+            var result = value.Left(10);
+
+            result.Should().BeEquivalentTo(value);
+        }
+
+        [Test]
+        public void Left_StringWithHigherLengthThanGivenLength_ShouldReturnLeftmostCharacters()
+        {
+            string value = "abcdefghijk";
+
+            var result = value.Left(5);
+
+            result.Should().BeEquivalentTo("abcde");
+        }
+
+        [Test]
+        public void Right_EmptyString_ShouldReturnEmptyString()
+        {
+            var value = "";
+
+            var result = value.Right(5);
+
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Right_NullString_ShouldReturnNullString()
+        {
+            string value = null;
+
+            var result = value.Right(5);
+
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void Right_StringWithLowerLengthThanGivenLength_ShouldReturnEntireString()
+        {
+            string value = "abcdef";
+
+            var result = value.Right(10);
+
+            result.Should().BeEquivalentTo(value);
+        }
+
+        [Test]
+        public void Right_StringWithHigherLengthThanGivenLength_ShouldReturnRightmostCharacters()
+        {
+            string value = "abcdefghijk";
+
+            var result = value.Right(5);
+
+            result.Should().BeEquivalentTo("ghijk");
+        }
+    }
+}
